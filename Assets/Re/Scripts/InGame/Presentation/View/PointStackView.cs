dbd46bacd3e0ec1fbdd159da84de89c27fdbb2a4
kitatas <315kitatas10703@gmail.com>
2022-12-18 16:14:46 +0900

    fix: PointStackView

--- Assets/Re/Scripts/InGame/Presentation/View/PointStackView.cs
+++ Assets/Re/Scripts/InGame/Presentation/View/PointStackView.cs
@@ -1,16 +1,31 @@
 using Re.Common.Presentation.View;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Re.InGame.Presentation.View
 {
     public sealed class PointStackView : BaseView<int>
     {
+        [SerializeField] private Image[] stockImages = default;
         [SerializeField] private TextMeshProUGUI countText = default;
 
         public override void Render(int value)
         {
-            countText.text = $"{value:000}";
+            for (int i = 0; i < stockImages.Length; i++)
+            {
+                stockImages[i].enabled = value > i;
+            }
+
+            if (value > stockImages.Length)
+            {
+                var lack = value - stockImages.Length;
+                countText.text = $"+{lack}";
+            }
+            else
+            {
+                countText.text = $"";
+            }
         }
     }
 }
